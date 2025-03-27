bbb160c69dafa8e8b80fe5f7235d8a769be800ef
Tyler <tylersora@gmail.com>
2025-03-26 23:38:26 -0600

    Changes to app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/quality-lawn-care/',
   server: {
     port: 3000,
     proxy: {
